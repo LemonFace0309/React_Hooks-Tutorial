dbea906a1c2e7e954d826d1f96fc2ad2d2ce7c68
LemonFace0309 <charles.liu0309@gmail.com>
2021-01-01 01:15:01 -0500

    added reducers for loading and error state

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useCallback } from 'react'
+import React, { useReducer, useCallback } from 'react'
 
 import ErrorModal from '../UI/ErrorModal'
 import IngredientForm from './IngredientForm'
@@ -17,15 +17,35 @@ const ingredientReducer = (currentIngredients, action) => {
       throw new Error('Something went wrong!')
   }
 }
+// reducers are useful for complex state that may change frequently or changes based on its previous state
+
+const httpReducer = (httpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return {loading: true, error: null,}
+    case 'RESPONSE':
+      return {...httpState, loading: false,}
+    case 'ERROR':
+      return {loading: false, error: action.errMsg}
+    case 'CLEAR':
+      return {...httpState, error: null,}
+    default:
+      throw new Error('Something went wrong!')
+  }
+}
+// reducers are also useful for states that are connected to each other
+
 
 const Ingredients = () => {
   const [ingredients, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {loading: false, error: null})
   // const [ingredients, setIngredients] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [error, setError] = useState(false)
+  // setState is batched when two more more setState are used in the same cycle as they both belong to the same synchronous event handler, causing only one rerender cycle
 
   const addIngredientHandler = (ingredient) => {
-    setIsLoading(true)
+    dispatchHttp({type: 'SEND'})
     fetch(
       'https://react-hooks-tutorial-hehe-default-rtdb.firebaseio.com/ingredients.json',
       {
@@ -36,7 +56,7 @@ const Ingredients = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        setIsLoading(false)
+        dispatchHttp({type: 'RESPONSE'})
         // setIngredients((prevIngredients) => [
         //   ...prevIngredients,
         //   { id: data.name, ...ingredient },
@@ -46,7 +66,7 @@ const Ingredients = () => {
   }
 
   const onRemoveIngredientHandler = (id) => {
-    setIsLoading(true)
+    dispatchHttp({type: 'SEND'})
     fetch(
       `https://react-hooks-tutorial-hehe-default-rtdb.firebaseio.com/ingredients/${id}.json`,
       {
@@ -54,36 +74,37 @@ const Ingredients = () => {
       }
     )
       .then((res) => {
-        setIsLoading(false)
+        dispatchHttp({type: 'RESPONSE'})
         // setIngredients((prevIngredients) => {
         //   return prevIngredients.filter((ig) => id !== ig.id)
         // })
         dispatch({ type: 'DELETE', id: id })
       })
       .catch((err) => {
-        setIsLoading(false)
-        setError(err.message)
-        // setState is batched here as they both belong to the same synchronous event handler, causing only one rerender cycle
+        dispatchHttp({type: 'ERROR', errMsg: err.message})
       })
   }
 
-  const filterIngredientsHandler = useCallback((filteredIngredients) => {
-    dispatch({ type: 'SET', ingredients: filteredIngredients })
-  }, [dispatch /* dispatch is a dependency, but will never change */])
+  const filterIngredientsHandler = useCallback(
+    (filteredIngredients) => {
+      dispatch({ type: 'SET', ingredients: filteredIngredients })
+    },
+    [dispatch /* dispatch is a dependency, but will never change */]
+  )
   // useCallback caches filterIngredientsHandler so it change after rerender cycles.
   // This is important because otherwise, useEffect in Search.js would be in an infinite loop
 
   const clearError = () => {
-    setError(null)
+    dispatchHttp({type: 'CLEAR'})
   }
 
   return (
     <div className="App">
-      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
 
       <IngredientForm
         onAddIngredient={addIngredientHandler}
-        loading={isLoading}
+        loading={httpState.loading}
       />
 
       <section>
